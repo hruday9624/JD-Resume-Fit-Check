ef27d6531a7ad4eb8c98046cdfec92984452d683
Hruday Mouly <Hruday96@users.noreply.huggingface.co>
2024-11-19 17:43:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 
+from docx import Document  # To handle .docx files
+import PyPDF2  # To handle .pdf files
 
 # App header
 st.header("JD-Resume-Fit-Check")
@@ -10,3 +12,12 @@ GOOGLE_API_KEY = st.secrets["GEMINI_API_KEY"]
 
 # Configure the Google Generative AI API with your API key
 genai.configure(api_key=GOOGLE_API_KEY)
+
+# Input field for the medicine name
+st.subheader("Enter Medicine Details:")
+medicine_name = st.text_input('Medicine Name', '')
+
+#Input field for the Resume
+st.subheader('Upload your Resume')
+# File upload for PDF or DOCX
+uploaded_file = st.file_uploader('Upload your Resume (PDF or DOCX)', type=['pdf', 'docx'])
